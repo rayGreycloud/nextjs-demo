00c2f5ec2a2ba497514b80c92e7f834500256b9a
rayGreycloud <rayGreycloud@gmail.com>
2018-06-14 18:05:04 -0700

    Add blog demo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Link from 'next/link';
 import Layout from '../components/MyLayout';
 
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
 const Index = () => (
   <Layout>
-    <p>HOME - this is the home page</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
   </Layout>
 );
 
