e9dc8e254c97fece718de6346c6d030cebf37359
rayGreycloud <rayGreycloud@gmail.com>
2018-06-14 19:48:07 -0700

    Refactor to fetch and display show info

--- pages/post.js
+++ pages/post.js
@@ -1,24 +1,24 @@
-import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout.js';
 
-const Content = withRouter(props => (
-  <div>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-));
-
-const Page = props => (
+const Post = props => (
   <Layout>
-    <Content />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
   </Layout>
 );
 
-// const Page = withRouter(props => (
-//   <Layout>
-//     <h1>{props.router.query.title}</h1>
-//     <p>This is the blog post content.</p>
-//   </Layout>
-// ));
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
 
-export default Page;
+  console.log(`Fetched show: ${show.name}`);
+
+  return {
+    show
+  };
+};
+
+export default Post;
